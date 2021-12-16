8c5fe47ece56747570ba98ee5d3864bdd370cae4
Steve Calvert <scalvert@linkedin.com>
2021-12-16 11:07:04 -0800

    Adds eslint configuration [shepherd]

---
+++ templates/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: false,
+    node: true,
+    es6: true,
+  },
+  plugins: ['node', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  }
+};
